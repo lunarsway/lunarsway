ca19c6a553a4c388193ff188d3b94815cdde205a
Preston Pope <preston@upgradedliving.com>
2008-08-09 17:29:43 -0700

    caching home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   layout "standard-layout"
   
+  caches_page :index
+  
   def index
     @news_items = NewsItem.recent
     @auth_token = authenticity_token_from_session_id
--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -65,6 +65,7 @@ class NewsItemsController < ApplicationController
 
     respond_to do |format|
       if @news_item.update_attributes(params[:news_item])
+        expire_page(:controller => "/home", :action => "index")
         flash[:notice] = 'NewsItem was successfully updated.'
         format.html { redirect_to(@news_item) }
         format.xml  { head :ok }
