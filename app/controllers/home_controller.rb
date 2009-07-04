f2f215b58007b46a5f155fa786ae87e39b8a0d71
Preston Pope <admin@reverenddanger.com>
2009-07-03 19:48:23 -0700

    removed auth thing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     @news_item = NewsItem.recent.first
     @news_items = Array.new
     @news_items << @news_item
-    @auth_token = authenticity_token_from_session_id
+    # @auth_token = authenticity_token_from_session_id
     
   end
   
