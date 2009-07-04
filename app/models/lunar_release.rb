93329469d6a1b8d8d7e1fe1b1f1d5d5a6aad7a88
Preston Pope <admin@reverenddanger.com>
2009-07-03 19:45:44 -0700

    removed acts_as_fleximage

--- app/models/lunar_release.rb
+++ app/models/lunar_release.rb
@@ -2,9 +2,9 @@ class LunarRelease < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 15
 
-  acts_as_fleximage do
-    image_directory 'lunar_releases'
+  #acts_as_fleximage do
+  #  image_directory 'lunar_releases'
 
-    image_storage_format :mp3
-  end
+  #  image_storage_format :mp3
+  #end
 end
