14ab89a3a63244b622411a7a8f757d736e2dad3b
Preston Pope <popeix@prestonpope.com>
2009-09-22 00:25:51 -0700

    tour date fi

--- app/models/tour_date.rb
+++ app/models/tour_date.rb
@@ -1,5 +1,5 @@
 class TourDate < ActiveRecord::Base
   def self.recent
-    self.find(:all, :conditions => ["show_date > ?", DateTime.now], :order => "show_date ASC")
+    self.find(:all, :conditions => ["show_date >= ?", DateTime.now], :order => "show_date ASC")
   end
 end
